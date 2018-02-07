8c742f22dd936fa0ac5d8d04575783008c48c4b2
zuzaszafran <zuza.szafranowska@g1ant.com>
2018-02-07 14:27:58 +0100

    Add license headers

--- G1ANT.Addon.Selenium.Tests/Commands/SeleniumSwitchTest.cs
+++ G1ANT.Addon.Selenium.Tests/Commands/SeleniumSwitchTest.cs
@@ -1,4 +1,13 @@
-﻿using G1ANT.Engine;
+﻿/**
+*    Copyright(C) G1ANT Ltd, All rights reserved
+*    Solution G1ANT.Addon, Project G1ANT.Addon.Selenium
+*    www.g1ant.com
+*
+*    Licensed under the G1ANT license.
+*    See License.txt file in the project root for full license information.
+*
+*/
+using G1ANT.Engine;
 using G1ANT.Language;
 using G1ANT.Language.Semantic;
 using NUnit.Framework;
--- G1ANT.Addon.Selenium.Tests/LoadSeleniumTest.cs
+++ G1ANT.Addon.Selenium.Tests/LoadSeleniumTest.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/**
+*    Copyright(C) G1ANT Ltd, All rights reserved
+*    Solution G1ANT.Addon, Project G1ANT.Addon.Selenium
+*    www.g1ant.com
+*
+*    Licensed under the G1ANT license.
+*    See License.txt file in the project root for full license information.
+*
+*/
+using System;
 using NUnit.Framework;
 
 namespace G1ANT.Addon.Selenium.Tests
