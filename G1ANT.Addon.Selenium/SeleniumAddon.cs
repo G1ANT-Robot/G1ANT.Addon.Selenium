df99f609001a7d4e959371f9b20a0da9fd14105a
Patryk Suchowierski <patryk.suchowierski@g1ant.com>
2018-02-09 13:57:20 +0100

    icons taken from resx fix

--- G1ANT.Addon.Selenium/SeleniumAddon.cs
+++ G1ANT.Addon.Selenium/SeleniumAddon.cs
@@ -19,11 +19,11 @@ using System.IO;
 
 namespace G1ANT.Addon.Selenium
 {
-    [Addon(Name = "Selenuium",
+    [Addon(Name = "Selenium",
         Tooltip = "Selenium Commands")]
     [Copyright(Author = "G1ANT LTD", Copyright = "G1ANT LTD", Email = "hi@g1ant.com", Website = "www.g1ant.com")]
     [License(Type = "LGPL", ResourceName = "License.txt")]
-    [CommandGroup(Name = "selenium", Tooltip = "Commands to work with web pages via supported web browsers.")]
+    [CommandGroup(Name = "selenium", Tooltip = "Commands to work with web pages via supported web browsers.", IconName = "seleniumicon")]
     public class SeleniumAddon : Language.Addon
     {
         public override void Initialize()
