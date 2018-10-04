719e7be07fc2c9b2e0c0f634938df034ef5b1003
Marcin Machowski <marcin.machowski@g1ant.com>
2018-10-04 13:23:17 +0200

    SeleniumCommandArguments 'text' option removed from tooltip

--- G1ANT.Addon.Selenium/Api/SeleniumCommandArguments.cs
+++ G1ANT.Addon.Selenium/Api/SeleniumCommandArguments.cs
@@ -7,7 +7,7 @@ namespace G1ANT.Addon.Selenium
         [Argument(Required = true, Tooltip = "Phrase to find element by")]
         public TextStructure Search { get; set; }
 
-        [Argument(Tooltip = "Specifies an element selector, possible values are: 'name', 'text', 'title', 'class', 'id', 'selector', 'query', 'jquery'")]
+        [Argument(Tooltip = "Specifies an element selector, possible values are: 'name', 'title', 'class', 'id', 'selector', 'query', 'jquery'")]
         public TextStructure By { get; set; } = new TextStructure(ElementSearchBy.Id.ToString().ToLower());
 
         [Argument(Tooltip = "Phrase to find iframe by")]
