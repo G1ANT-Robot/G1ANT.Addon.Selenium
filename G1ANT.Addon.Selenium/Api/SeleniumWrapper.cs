d684f26dbcbe50e652c57544c72175df741c197d
Daniel Kaiser <danieladamkaiser@gmail.com>
2019-01-03 12:09:49 +0100

    Added GetText Command

--- G1ANT.Addon.Selenium/Api/SeleniumWrapper.cs
+++ G1ANT.Addon.Selenium/Api/SeleniumWrapper.cs
@@ -368,6 +368,18 @@ namespace G1ANT.Addon.Selenium
             return res;
         }
 
+        public string GetTextValue(SeleniumCommandArguments search, TimeSpan timeout)
+        {
+            PreCheckCurrentWindowHandle();
+            if (string.IsNullOrEmpty(search.IFrameSearch?.Value) == false)
+                webDriver.SwitchTo().Frame(FindElement(search.IFrameSearch.Value, search.IFrameBy.Value, timeout));
+            IWebElement element = FindElement(search.Search.Value, search.By.Value, timeout);
+            string res = element?.Text ?? string.Empty;
+            if (string.IsNullOrEmpty(search.IFrameSearch?.Value) == false)
+                webDriver.SwitchTo().DefaultContent();
+            return res;
+        }
+
         public void SetAttributeValue(string attributeName, string attributeValue, SeleniumCommandArguments search, TimeSpan timeout)
         {
             PreCheckCurrentWindowHandle();
