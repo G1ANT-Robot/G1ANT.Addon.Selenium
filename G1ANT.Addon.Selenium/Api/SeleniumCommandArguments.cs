5ad66bfd75153eb8766d239db346e18f8722674e
Wiktoria Prusik <39299658+WiktoriaPrusik@users.noreply.github.com>
2019-04-26 11:49:54 +0200

    Update SeleniumCommandArguments.cs

--- G1ANT.Addon.Selenium/Api/SeleniumCommandArguments.cs
+++ G1ANT.Addon.Selenium/Api/SeleniumCommandArguments.cs
@@ -4,16 +4,16 @@ namespace G1ANT.Addon.Selenium
 {
     public class SeleniumCommandArguments : CommandArguments
     {
-        [Argument(Required = true, Tooltip = "Phrase to find element by")]
+        [Argument(Required = true, Tooltip = "Phrase to find an element by")]
         public TextStructure Search { get; set; }
 
-        [Argument(Tooltip = "Specifies an element selector, possible values are: 'name', 'title', 'class', 'id', 'selector', 'query', 'jquery'")]
+        [Argument(Tooltip = "Specifies an element selector: 'id', 'class', 'cssselector', 'tag', 'xpath', 'name', 'query', 'jquery'")]
         public TextStructure By { get; set; } = new TextStructure(ElementSearchBy.Id.ToString().ToLower());
 
-        [Argument(Tooltip = "Phrase to find iframe by")]
+        [Argument(Tooltip = "Phrase to find an iframe by")]
         public TextStructure IFrameSearch { get; set; }
 
-        [Argument(Tooltip = "Specifies an element selector for iframe search, possible values are: 'name', 'title', 'class', 'id', 'selector', 'query', 'jquery'")]
+        [Argument(Tooltip = "Specifies an iframe selector: 'id', 'class', 'cssselector', 'tag', 'xpath', 'name', 'query', 'jquery'")]
         public TextStructure IFrameBy { get; set; } = new TextStructure(ElementSearchBy.Id.ToString().ToLower());
     }
 }
