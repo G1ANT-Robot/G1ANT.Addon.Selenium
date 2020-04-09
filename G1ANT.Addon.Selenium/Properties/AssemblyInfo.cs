db201cf46e109eab77d1e90db1d257cc19655ec4
Łukasz Fronczyk <lukasz.fronczyk@g1ant.com>
2020-04-09 11:23:36 +0200

    updated selenium browser drivers

--- G1ANT.Addon.Selenium/Properties/AssemblyInfo.cs
+++ G1ANT.Addon.Selenium/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("G1ANT.Addon.Selenium")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
